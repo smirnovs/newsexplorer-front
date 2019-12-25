c50e2b611cb7212d0cfd06184763101b3c2813db
Alexandr Smirnov <uzukwert@gmail.com>
2019-12-25 18:02:14 +0300

    popup start

--- src/js/popup/popup.js
+++ src/js/popup/popup.js
@@ -2,7 +2,15 @@ const authButton = document.querySelector('.header__login');
 const popupCloseButton = document.querySelector('.popup__close');
 const popUp = document.querySelector('.popup');
 const mobileAuth = document.querySelector('.menumobile__login');
-
+const toRegister = document.querySelector('.popup__reg');
+// const offerText = document.querySelector('.popup__offer');
+const popupTitle = document.querySelector('.popup__title');
+const popupInputNameSbt = document.querySelector('.popup__input_name-sbt');
+const popupInputName = document.querySelector('.popup__input_name');
+const popupErrorUser = document.querySelector('.popup__error_user');
+const toLogin = document.querySelector('.popup__auth');
+const popupEnter = document.querySelector('.popup__button_enter');
+const popupReg = document.querySelector('.popup__button_reg');
 
 export class Popup {
     constructor() {
@@ -14,10 +22,34 @@ export class Popup {
     close() {
         popUp.classList.toggle('popup_is-opened');
     }
+    toregister() {
+        popupTitle.textContent = 'Регистрация';
+        toRegister.style.display = 'none'
+        popupEnter.style.display = 'none'
+        popupReg.style.display = 'inline-block'
+        toLogin.style.display = 'inline-block';
+        display('inline-block');
+    }
+    tologin() {
+        console.log('slasd')
+        popupTitle.textContent = 'Вход';
+        toRegister.style.display = 'inline-block'
+        toLogin.style.display = 'none';
+        popupReg.style.display = 'none'
+        popupEnter.style.display = 'inline-block'
+        display('none');
+    }
     addListeners() {
         authButton.addEventListener('click', this.open);
         mobileAuth.addEventListener('click', this.open);
         popupCloseButton.addEventListener('click', this.close);
+        toRegister.addEventListener('click', this.toregister);
+        toLogin.addEventListener('click', this.tologin);
     }
 }
 
+const display = (status) => {
+    popupInputNameSbt.style.display = status;
+    popupInputName.style.display = status;
+    popupErrorUser.style.display = status;
+}
