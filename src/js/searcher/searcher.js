1b821dc2984af3073243cc3145b8e1c6cac10d7c
Alexandr Smirnov <uzukwert@gmail.com>
2019-12-25 16:36:37 +0300

    load 3

--- src/js/searcher/searcher.js
+++ src/js/searcher/searcher.js
@@ -9,66 +9,81 @@ const preloader = document.querySelector('.preloader')
 const preloaderAwait = document.querySelector('.preloader__await-block');
 const preloaderNotfound = document.querySelector('.preloader__notfound-block');
 const searchResult = document.querySelector('.search-result__container');
-const showMore = document.querySelector('.search-result__button');
+// const showMore = document.querySelector('.search-result__button');
 const cardContainer = document.querySelector('.search-result__cards');
 // const cardContainer = document.querySelector('.search-result__container');
+let isShowMore = false;
 
-
+let n = 3;
 export class Search {
-    constructor() {
-        // this.search = this.search.bind(this);
-        // this.addListeners();
-        // this.find = this.find.bind(this);
+    showmorebutton() {
+        const showMore = document.createElement('button');
+        showMore.classList.add('button');
+        showMore.classList.add('search-result__button');
+        showMore.textContent = 'Показать еще';
+        searchResult.appendChild(showMore);
     }
     find() {
-        // console.log('1223123')
         event.preventDefault();
         startSearchStyling();
         let myQuestion = question.value;
-        console.log(myQuestion);
         api.getCards(myQuestion).then(cards => {
             if (cards.totalResults === 0) {
                 preloaderAwait.style.display = 'none';
                 preloaderNotfound.style.display = 'flex';
             } else {
                 successSearchStyling(cards);
-                // console.log(cards.totalResults)
-                console.log(cards.articles)
-                console.log(cards)
-                // for (let i = 0; i < 3; i++) {
-                //     console.log(cards.articles);
-                //     const { cardElement } = new Card(cards.articles[i].urlToImage, cards.articles[i].publishedAt, cards.articles[i].title, cards.articles[i].description, cards.articles[i].source.name);
-                //     cardContainer.appendChild(cardElement);
-                // }
-                // showMore.addEventListener('click', () => {
-                //     if (cards.articles.length < 6) {
-                //         let n = cards.articles.length;
-                //         return n;
-                //     }
-                //     for (let i = 3; i < n; i++) {
-                //         console.log(cards.articles);
-                //         const { cardElement } = new Card(cards.articles[i].urlToImage, cards.articles[i].publishedAt, cards.articles[i].title, cards.articles[i].description, cards.articles[i].source.name);
-                //         cardContainer.appendChild(cardElement);
-                //         console.log(cards.articles.length);
-                //     }
-                // });
-                for (let card of cards.articles) {
-                    // console.log(card.source.name)
-                    // console.log(card)
-                    const { cardElement } = new Card(card.urlToImage, card.publishedAt, card.title, card.description, card.source.name);
-                    // console.log(cardElement)
-                    // console.log(card)
-                    let cardList = [];
-                    // console.log(cardList);
-                    cardList.push(cardElement)
-                    // console.log(cardList.length);
-                    return cardList;
-                    // cardContainer.appendChild(cardElement);
-
+                if (cards.articles.length < 3) {
+                    let n = cards.articles.length
+                    for (let i = 0; i < n; i++) {
+                        const { cardElement } = new Card(cards.articles[i].urlToImage, cards.articles[i].publishedAt, cards.articles[i].title, cards.articles[i].description, cards.articles[i].source.name);
+                        cardContainer.appendChild(cardElement);
+                    }
+                } else {
+                    for (let i = 0; i < 3; i++) {
+                        const { cardElement } = new Card(cards.articles[i].urlToImage, cards.articles[i].publishedAt, cards.articles[i].title, cards.articles[i].description, cards.articles[i].source.name);
+                        cardContainer.appendChild(cardElement);
+                    }
                 }
+                if (document.querySelector('.search-result__button')) {
+                    const showMore = document.querySelector('.search-result__button');
+                    let n = 3;
+                    showMore.addEventListener('click', function showMoreHandler() {
+                        if (cards.articles.length <= 6) {
+                            for (let i = 3; i < n; i++) {
+                                const { cardElement } = new Card(cards.articles[i].urlToImage, cards.articles[i].publishedAt, cards.articles[i].title, cards.articles[i].description, cards.articles[i].source.name);
+                                cardContainer.appendChild(cardElement);
+                                showMore.style.display = 'none'
+                                n = 3;
+                            }
+                        } else if (cards.articles.length > 6) {
+                            if (n + 3 > cards.articles.length) {
+                                let s = cards.articles.length;
+                                for (let i = n; i < s; i++) {
+                                    const { cardElement } = new Card(cards.articles[i].urlToImage, cards.articles[i].publishedAt, cards.articles[i].title, cards.articles[i].description, cards.articles[i].source.name);
+                                    cardContainer.appendChild(cardElement);
+                                }
+                            } else {
+                                let s = n + 3;
+                                for (let i = n; i < s; i++) {
+                                    const { cardElement } = new Card(cards.articles[i].urlToImage, cards.articles[i].publishedAt, cards.articles[i].title, cards.articles[i].description, cards.articles[i].source.name);
+                                    cardContainer.appendChild(cardElement);
+                                }
+                            }
 
-
+                            n = n + 3;
+                            if (n >= cards.articles.length) {
+                                showMore.style.display = 'none';
+                                showMore.removeEventListener('click', showMoreHandler);
+                                n = 3;
+                            }
+                        }
+                    })
+                }
             }
+        }).catch(() => {
+            preloaderAwait.style.display = 'none';
+            preloaderNotfound.style.display = 'flex';
         });
     }
 }
@@ -80,7 +95,10 @@ const startSearchStyling = () => {
         cardContainer.removeChild(cardContainer.lastChild);
     }
     preloaderNotfound.style.display = 'none';
-    showMore.style.display = 'none';
+    if (document.querySelector('.search-result__button')) {
+        const showMore = document.querySelector('.search-result__button');
+        showMore.parentNode.removeChild(showMore);
+    }
     preloader.style.display = 'flex';
     preloaderAwait.style.display = 'flex';
 };
@@ -89,7 +107,7 @@ const successSearchStyling = (cards) => {
     preloader.style.display = 'none';
     searchResult.style.display = 'flex';
     if (cards.totalResults > 3) {
-        showMore.style.display = 'inline-block';
+        search.showmorebutton();
     }
 }
 
